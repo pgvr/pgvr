ec959460905eceedbb77d1177695bfb8001b966e
Patrick Göler von Ravensburg <patrick@productlane.com>
2023-12-17 23:10:34 +0100

    update

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -65,8 +65,7 @@ export default function About() {
           </div>
           <div className="lg:order-first lg:row-span-2">
             <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-              I’m Patrick, I live in Munich, where I help to build the carbon
-              accounting platform of the future.
+              Hi, I’m Patrick 👋
             </h1>
             <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
               <p>
